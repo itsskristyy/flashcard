9dd91d7b68d9ebb5c44cb6e2f85c5ad6e68c0cc1
kristy delacruz <kristydelacruzx@gmail.com>
2022-09-08 19:19:42 -0400

    Initial Commit

---
+++ flashcard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flashcard
+//
+//  Created by kristy delacruz on 9/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
